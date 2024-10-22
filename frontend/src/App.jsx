56dd05dff88947ec0afb060509f97e4660bc8ca8
rohit kedar <rohitkedar2000@gmail.com>
2024-10-22 11:13:05 +0530

    completed responsive Nav bar ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
     <>
-    <nav>Nav bar</nav>
-<main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6'>
+    <Navbar/>
+<main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
 
   <Outlet/>
 </main>
